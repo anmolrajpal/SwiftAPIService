b52f3b3c06285a084826fddfcab6ea26bb823f4c
Anmol Rajpal <anmol.rajpal@me.com>
2024-05-19 18:08:17 +0530

    Bug fix

--- Package.swift
+++ Package.swift
@@ -7,19 +7,27 @@ let package = Package(
     name: "SwiftAPIService",
     platforms: [
       .iOS(.v15),
-      .macOS(.v12)
+      .macOS(.v12),
+      .tvOS(.v12),
+      .watchOS(.v4)
     ],
+    
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftAPIService",
             targets: ["SwiftAPIService"]),
+        
+         .plugin(
+             name: "GenerateHelpers",
+             targets: ["GenerateHelpers"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftAPIService"),
+            name: "SwiftAPIService"
+        ),
         
          .plugin(
             name: "GenerateHelpers",
--- Plugins/GenerateHelpers/GenerateHelpersPlugin.swift
+++ Plugins/GenerateHelpers/GenerateHelpersPlugin.swift
@@ -1,3 +1,4 @@
+
 import PackagePlugin
 import Foundation
 
